24572bf2639d8e46b769ef11ceb100b99b0b4930
nickjbaum <33940466+nickjbaum@users.noreply.github.com>
2017-11-27 16:43:13 -0600

    Added comments

--- SemProj.py
+++ SemProj.py
@@ -4,6 +4,7 @@ import time
 import smtplib
 import os
 
+#setting up smtp
 server = smtplib.SMTP('smtp.gmail.com',587)
 server.ehlo()
 server.starttls()
@@ -11,6 +12,7 @@ server.ehlo()
 server.login("<user>","<pass>")
 msg = 'The tank is half full. Please refill.'
 
+#configure board pin numbering system for Pi
 GPIO.setmode(GPIO.BOARD)
 
 #Pin numbering
@@ -18,13 +20,17 @@ BUTTON = 7
 TRIG = 38
 ECHO = 40
 
+#setting push button as a pull down resistor
 GPIO.setup(BUTTON,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 
+#setting sensor trigger pin as an output
 GPIO.setup(TRIG,GPIO.OUT)
 GPIO.output(TRIG,0)
 
+#setting sensor echo pin as an input
 GPIO.setup(ECHO,GPIO.IN)
 
+#the device starts first measurement when button is pressed
 while GPIO.input(BUTTON) == 0:
     pass
 
@@ -42,13 +48,16 @@ while GPIO.input(ECHO) == 1:
     pass
 stop = time.time()
 
+#saving initial measurement as a variable
 distance1 = (stop-start) * 170
 string1 = str(distance1)
 print('Tank height: ' + string1[0:4] + ' meters')
 
+#continuous measurement begins when button is pressed a second time
 while GPIO.input(BUTTON) == 0:
     pass
 
+#storing subsequent measurements as a variable
 distance2 = 0
 
 while distance2 < (distance1 / 2):
@@ -74,9 +83,11 @@ while distance2 < (distance1 / 2):
     string2 = str(distance2)
     print('Current water height: '+ string2[0:4] +' meters')
     
+#when tank is half full, send an email with the message
 print('Magic Time!')
 server.sendmail("sender","receiver",msg)
 server.quit()
 
+#reset and shut down Pi after email is sent
 GPIO.cleanup()
 os.system('sudo shutdown -h now')
